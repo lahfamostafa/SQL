cba52caf40832801ab2e73c1c8509c2ac0624ec6
lahfamostafa <lahfa.mostafa1@gmail.com>
2025-12-03 14:49:15 +0100

    Creation da la base de données et la premier table (Customers)

---
+++ db.sql
@@ -0,0 +1,11 @@
+create database Bankly
+
+use Bankly
+
+create table if not exists Customers (
+    customer_id int primary key,
+    full_name varchar (30),
+    email varchar (40),
+    phone varchar(10),
+    registration_date date 
+);
