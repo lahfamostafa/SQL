7f1edbb3caa6a5396a36db9efe543f00a1d723de
lahfamostafa <lahfa.mostafa1@gmail.com>
2025-12-03 16:58:15 +0100

    querys number 1 , 2 & 3

---
+++ querys.sql
@@ -0,0 +1,8 @@
+/*      1       */
+insert into Customers values (6, 'Ghizlaine','email','0699887766','2007-09-28')
+
+/*      2       */
+update Customers set phone ='0677675444' where full_name ='med kassir'
+
+/*      3       */
+select * from Customers 
